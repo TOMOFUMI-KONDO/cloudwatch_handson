e909a28d683e13ce3066e7a0e55a7262dd4740c8
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-04-23 09:11:05 +0900

    Initial commit

---
+++ test/cloudwatch_handson.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CloudwatchHandson from '../lib/cloudwatch_handson-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cloudwatch_handson-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CloudwatchHandson.CloudwatchHandsonStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
