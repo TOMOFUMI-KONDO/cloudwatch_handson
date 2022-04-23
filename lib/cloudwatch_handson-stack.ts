838afd08a7df2b16e1c8b3f9e9d209dab681b5a7
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-04-23 10:10:54 +0900

    feat: update s3 bucket

--- lib/cloudwatch_handson-stack.ts
+++ lib/cloudwatch_handson-stack.ts
@@ -1,16 +1,14 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib"
+import { aws_s3 as s3 } from "aws-cdk-lib"
 
-export class CloudwatchHandsonStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+export class CloudwatchHandsonStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CloudwatchHandsonQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3.Bucket(this, "MyFirstBucket", {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    })
   }
 }
