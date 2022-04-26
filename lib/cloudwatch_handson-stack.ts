9689fa3cb17f3cbc582b18b530ee57651b1db68a
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-04-27 08:38:01 +0900

    feat: add EC2 instance

--- lib/cloudwatch_handson-stack.ts
+++ lib/cloudwatch_handson-stack.ts
@@ -1,11 +1,11 @@
 import * as cdk from "aws-cdk-lib"
-import { aws_ec2 as ec2 } from "aws-cdk-lib"
+import { aws_ec2 as ec2, aws_iam as iam } from "aws-cdk-lib"
 
 export class CloudwatchHandsonStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    new ec2.Vpc(this, "VPC", {
+    const vpc = new ec2.Vpc(this, "VPC", {
       natGateways: 0,
       subnetConfiguration: [
         {
@@ -19,6 +19,28 @@ export class CloudwatchHandsonStack extends cdk.Stack {
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
         },
       ],
+    })
+
+    const securityGroup = new ec2.SecurityGroup(this, "PublicInstanceSG", {
+      vpc,
+      description: "For public instance",
+      allowAllOutbound: true,
+    })
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), "Allow public http")
+
+    const role = new iam.Role(this, "PublicInstanceRole", { assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com") })
+    role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore"))
+
+    new ec2.Instance(this, "PublicInstance", {
+      vpc,
+      vpcSubnets: vpc.selectSubnets({ subnetType: ec2.SubnetType.PRIVATE_ISOLATED }),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.NANO),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+        cpuType: ec2.AmazonLinuxCpuType.ARM_64,
+      }),
+      securityGroup,
+      role,
     })
   }
 }
