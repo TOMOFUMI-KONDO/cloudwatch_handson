b8367c9f48c03cf7ff54d3c28f367dc50bdf606d
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-04-27 23:16:01 +0900

    feat: add ALB connection to EC2 instance

--- lib/cloudwatch_handson-stack.ts
+++ lib/cloudwatch_handson-stack.ts
@@ -1,5 +1,10 @@
 import * as cdk from "aws-cdk-lib"
-import { aws_ec2 as ec2, aws_iam as iam } from "aws-cdk-lib"
+import {
+  aws_ec2 as ec2,
+  aws_elasticloadbalancingv2 as elb,
+  aws_elasticloadbalancingv2_targets as targets,
+  aws_iam as iam,
+} from "aws-cdk-lib"
 
 export class CloudwatchHandsonStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -21,26 +26,63 @@ export class CloudwatchHandsonStack extends cdk.Stack {
       ],
     })
 
-    const securityGroup = new ec2.SecurityGroup(this, "PublicInstanceSG", {
+    const securityGroupEC2 = new ec2.SecurityGroup(this, "SecurityGroupEC2", {
       vpc,
-      description: "For public instance",
+      description: "For ec2 instance",
       allowAllOutbound: true,
     })
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), "Allow public http")
 
     const role = new iam.Role(this, "PublicInstanceRole", { assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com") })
     role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore"))
 
-    new ec2.Instance(this, "PublicInstance", {
+    const userData = ec2.UserData.forLinux({ shebang: "#!/bin/bash" })
+    userData.addCommands(
+      "yum update -y",
+      "yum install -y httpd",
+      "systemctl start httpd.service",
+      "systemctl enable httpd.service",
+      "curl http://169.254.169.254/latest/meta-data/ > /var/www/html/index.html"
+    )
+
+    const instance = new ec2.Instance(this, "PublicInstance", {
       vpc,
-      vpcSubnets: vpc.selectSubnets({ subnetType: ec2.SubnetType.PRIVATE_ISOLATED }),
+      vpcSubnets: vpc.selectSubnets({ subnetType: ec2.SubnetType.PUBLIC }),
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.NANO),
       machineImage: new ec2.AmazonLinuxImage({
         generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
         cpuType: ec2.AmazonLinuxCpuType.ARM_64,
       }),
-      securityGroup,
+      securityGroup: securityGroupEC2,
       role,
+      userData,
     })
+
+    const securityGroupALB = new ec2.SecurityGroup(this, "SecurityGroupALB", {
+      vpc,
+      description: "For public ALB",
+      allowAllOutbound: true,
+    })
+    securityGroupALB.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), "Allow public HTTP access")
+
+    const alb = new elb.ApplicationLoadBalancer(this, "ALB", {
+      vpc,
+      internetFacing: true,
+      vpcSubnets: vpc.selectSubnets({ subnetType: ec2.SubnetType.PUBLIC }),
+      securityGroup: securityGroupALB,
+    })
+
+    const listener = alb.addListener("Listener", { port: 80 })
+    listener.addTargets("Target", {
+      port: 80,
+      targets: [new targets.InstanceTarget(instance)],
+    })
+
+    securityGroupEC2.addIngressRule(
+      ec2.Peer.securityGroupId(securityGroupALB.securityGroupId),
+      ec2.Port.tcp(80),
+      "Allow HTTP access from ALB"
+    )
+
+    new cdk.CfnOutput(this, "ALB DNS name", { value: alb.loadBalancerDnsName })
   }
 }
