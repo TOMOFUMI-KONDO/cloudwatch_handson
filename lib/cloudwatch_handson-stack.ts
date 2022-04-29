f032b939ef8a36e7165b548128771422857f368f
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-04-29 10:03:37 +0900

    feat: add rds instance

--- lib/cloudwatch_handson-stack.ts
+++ lib/cloudwatch_handson-stack.ts
@@ -4,6 +4,10 @@ import {
   aws_elasticloadbalancingv2 as elb,
   aws_elasticloadbalancingv2_targets as targets,
   aws_iam as iam,
+  aws_rds as rds,
+  aws_secretsmanager as secretsmanager,
+  Duration,
+  RemovalPolicy,
 } from "aws-cdk-lib"
 
 export class CloudwatchHandsonStack extends cdk.Stack {
@@ -82,6 +86,63 @@ export class CloudwatchHandsonStack extends cdk.Stack {
       ec2.Port.tcp(80),
       "Allow HTTP access from ALB"
     )
+
+    const dbName = "MysqlDatabase"
+    const dbUserName = "dbadmin"
+
+    const dbSecret = new secretsmanager.Secret(this, "DatabaseCredentials", {
+      secretName: dbName + "DatabaseCredentials",
+      description: dbName + "Database Credentials",
+      generateSecretString: {
+        excludeCharacters: "\"@/\\ '",
+        generateStringKey: "password",
+        passwordLength: 30,
+        secretStringTemplate: JSON.stringify({ username: dbUserName }),
+      },
+    })
+    const dbCredentials = rds.Credentials.fromSecret(dbSecret, dbUserName)
+
+    const dbsg = new ec2.SecurityGroup(this, "DatabaseSecurityGroup", {
+      vpc,
+      description: "For Mysql Database",
+      allowAllOutbound: true,
+    })
+    dbsg.addIngressRule(
+      ec2.Peer.securityGroupId(securityGroupEC2.securityGroupId),
+      ec2.Port.tcp(3306),
+      "Allow mysql connection from app server"
+    )
+
+    const privateSubnets = vpc.selectSubnets({ subnetType: ec2.SubnetType.PRIVATE_ISOLATED })
+    const dbSubnetGroup = new rds.SubnetGroup(this, "DatabaseSubnetGroup", {
+      vpc,
+      description: "Database Subnet Group",
+      vpcSubnets: privateSubnets,
+    })
+
+    new rds.DatabaseInstance(this, "MysqlDatabase", {
+      databaseName: dbName,
+      instanceIdentifier: dbName,
+      credentials: dbCredentials,
+      engine: rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_8_0_28 }),
+      backupRetention: Duration.days(7),
+      allocatedStorage: 20,
+      securityGroups: [dbsg],
+      allowMajorVersionUpgrade: true,
+      autoMinorVersionUpgrade: true,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.MICRO),
+      vpcSubnets: privateSubnets,
+      vpc,
+      removalPolicy: RemovalPolicy.DESTROY,
+      storageEncrypted: true,
+      monitoringInterval: Duration.seconds(60),
+      enablePerformanceInsights: true,
+      subnetGroup: dbSubnetGroup,
+      preferredBackupWindow: "00:15-01:15",
+      preferredMaintenanceWindow: "Sun:23:45-Mon:00:15",
+      publiclyAccessible: false,
+      multiAz: true,
+    })
 
     new cdk.CfnOutput(this, "ALB DNS name", { value: alb.loadBalancerDnsName })
   }
